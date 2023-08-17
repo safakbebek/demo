ad7c6d14ec479bc0b4a962ae8ca4aafad0db84d6
safakbebek <safak.bebek@harpeon.com>
2023-08-17 21:09:26 +0300

    test

---
+++ app2/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+// middleware
+
+app.get("/",(req,res)=> {
+  return res.send("Hi");
+})
+// Require the routers
+
+
+// use the routes
+
+app.listen(3000);
